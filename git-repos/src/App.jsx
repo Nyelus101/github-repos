e3c529679814a941ee9a10a231616e4cc5a8cbc1
Nyelus101 <cmuonyelu@gmail.com>
2024-05-07 22:23:16 +0100

    update

--- git-repos/src/App.jsx
+++ git-repos/src/App.jsx
@@ -1,17 +1,20 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RepoDetail from './components/RepoDetail';
-import RepoList from './components/RepoList';
+// import RepoDetail from './components/RepoDetail';
+// import RepoList from './components/RepoList';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact component={RepoList} />
-        <Route path="/repositories/:repoName" component={RepoDetail} />
-      </Routes>
-    </Router>
+
+
+    <h1>Hello world</h1>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" exact component={RepoList} />
+    //     <Route path="/repositories/:repoName" component={RepoDetail} />
+    //   </Routes>
+    // </Router>
   );
 };
 
